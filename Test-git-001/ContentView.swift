1a7f95514e04c1fd3126d00c63f5db72d96ac192
Parinthon Muninthon <parinthonboss@gmail.com>
2024-03-30 13:56:53 +0700

    Initial Commit

---
+++ Test-git-001/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test-git-001
+//
+//  Created by Parinthon Muninthon on 30/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test-git-001/Test_git_001App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_git_001App.swift
+//  Test-git-001
+//
+//  Created by Parinthon Muninthon on 30/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Test_git_001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
